d4a99f0c4c318503c41246cd6da6662a04a6d5e8
Lauren Clark <lauren.c4231@gmail.com>
2022-05-24 23:06:05 +0100

    Spit out details from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import useFetch from "./hooks/useFetch";
+
 function App() {
-    return <div></div>;
+    const url = "https://fakestoreapi.com/products";
+    const { response, error, isLoading } = useFetch(url);
+    return (
+        <div>
+            {error && "error"}
+            {isLoading && "Loading..."}
+            {response &&
+                response.map(({ id, title, description, image, category }) => (
+                    <ul key={id}>
+                        <li>
+                            <img src={image} alt={description} />
+                        </li>
+                        <li>{title}</li>
+                        <li>{description}</li>
+                        <li>{category}</li>
+                    </ul>
+                ))}
+        </div>
+    );
 }
 
 export default App;
