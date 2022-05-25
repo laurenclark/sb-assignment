c0575f2e6ba63f9947bc856302b3697ad52b0b2e
Lauren Clark <lauren.c4231@gmail.com>
2022-05-25 20:23:16 +0100

    Refactor

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,45 +1,5 @@
-import styled from "styled-components";
+import { ProductItem, ProductImage, ProductDescription } from "./ProductStyles";
 import Favourite from "./Favourite";
-
-const ProductItem = styled.li`
-    width: 42%;
-    padding: 20px;
-    @media (min-width: 768px) {
-        width: 31%;
-        padding: 1%;
-    }
-`;
-
-const ProductImage = styled.img`
-    display: block;
-    max-width: 100%;
-    margin: 0 auto;
-    object-fit: contain;
-    height: 350px;
-    width: 200px;
-    @media (min-width: 768px) {
-        height: 500px;
-        width: 300px;
-    }
-`;
-
-const ProductDescription = styled.div`
-    margin-top: 20px;
-    line-height: 1.2;
-    min-height: 130px;
-    h4,
-    p {
-        margin-bottom: 15px;
-    }
-    p:last-of-type {
-        text-transform: capitalize;
-    }
-    h4 {
-        display: block;
-        padding-right: 40px;
-        position: relative;
-    }
-`;
 
 export default function Product({ title, description, image, category }) {
     return (
---
+++ src/components/ProductStyles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const ProductItem = styled.li`
+    width: 42%;
+    padding: 20px;
+    @media (min-width: 768px) {
+        width: 31%;
+        padding: 1%;
+    }
+`;
+
+export const ProductImage = styled.img`
+    display: block;
+    max-width: 100%;
+    margin: 0 auto;
+    object-fit: contain;
+    height: 350px;
+    width: 200px;
+    @media (min-width: 768px) {
+        height: 500px;
+        width: 300px;
+    }
+`;
+
+export const ProductDescription = styled.div`
+    margin-top: 20px;
+    line-height: 1.2;
+    min-height: 130px;
+    h4,
+    p {
+        margin-bottom: 15px;
+    }
+    p:last-of-type {
+        text-transform: capitalize;
+    }
+    h4 {
+        display: block;
+        padding-right: 40px;
+        position: relative;
+    }
+`;
